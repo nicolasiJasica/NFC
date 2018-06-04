f6af030b8dbd1ba3a30c2177a0a853a4ed31294f
weiliang <weiliangliang@sogou-inc.com>
2018-06-04 10:40:19 +0800

    test

---
+++ Script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  Script.sh
+#  
+#
+#  Created by weiliang on 2018/4/3.
+#
+
+#chmod 777 /Users/liangwei/Desktop/CodeSnippets/
+
+
+cp -r ~/Library/Developer/Xcode/UserData/CodeSnippets/   /Users/liangwei/Desktop/aaa
+
+echo "copy success"
+
